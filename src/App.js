479573ac80003cc2757cf9043a05e0893d1b42a1
elifcifci <eliffbat@gmail.com>
2022-08-11 00:52:59 +0300

    added condition to appear of scroll button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import GlobalStyle from "./styles/globalStyles";
 import { AnimateSharedLayout } from "framer-motion";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Scroll from "./Components/Scroll/index";
 
 function App() {
@@ -16,13 +16,12 @@ function App() {
     <div className="container">
       {/* Common variables such as styles and color that apply to the entire project are in the global file. */}
       <GlobalStyle />
-      <Scroll scrollPoint={66} ScrollIconAppearPoint={500} direction={"scrollUp"} />
-      <Scroll scrollPoint={99999} ScrollIconAppearPoint={65} direction={"scrollDown"} />
-
       {/* The AnimateSharedLayout component enables you to perform layout animations between different components that share a layoutId as they're added/removed. */}
       <AnimateSharedLayout>
         <Router>
           <Navbar />
+          <Scroll scrollPoint={66} ScrollIconAppearPoint={500} direction={"scrollUp"} />
+          <Scroll scrollPoint={99999} ScrollIconAppearPoint={65} direction={"scrollDown"} />
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/about-page" exact element={<About />} />
--- src/Components/Scroll/index.js
+++ src/Components/Scroll/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { ScrollIcon } from "./styles";
 import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 const Scroll = ({ scrollPoint, ScrollIconAppearPoint, direction }) => {
   const [visible, setVisible] = useState(false);
+  let location = useLocation();
 
   useEffect(() => {
     window.addEventListener("scroll", () => {
@@ -43,19 +45,25 @@ const Scroll = ({ scrollPoint, ScrollIconAppearPoint, direction }) => {
     });
   };
 
-  return (
-    <>
-      {visible && (
-        <ScrollIcon
-          onClick={scrollThisPoint}
-          viewBox="0 0 510 510"
-          direction={direction}
-        >
-          <path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM390.6 310.6c-12.5 12.5-32.75 12.5-45.25 0L256 221.3L166.6 310.6c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l112-112C239.6 147.1 247.8 144 256 144s16.38 3.125 22.62 9.375l112 112C403.1 277.9 403.1 298.1 390.6 310.6z" />
-        </ScrollIcon>
-      )}
-    </>
-  );
+  if (
+    location.pathname === "/project-page" ||
+    location.pathname === "/article-page"
+  ) {
+    return (
+      <>
+        {visible && (
+          <ScrollIcon
+            onClick={scrollThisPoint}
+            viewBox="0 0 510 510"
+            direction={direction}
+          >
+            <path d="M256 0C114.6 0 0 114.6 0 256c0 141.4 114.6 256 256 256s256-114.6 256-256C512 114.6 397.4 0 256 0zM390.6 310.6c-12.5 12.5-32.75 12.5-45.25 0L256 221.3L166.6 310.6c-12.5 12.5-32.75 12.5-45.25 0s-12.5-32.75 0-45.25l112-112C239.6 147.1 247.8 144 256 144s16.38 3.125 22.62 9.375l112 112C403.1 277.9 403.1 298.1 390.6 310.6z" />
+          </ScrollIcon>
+        )}
+      </>
+    );
+  }
+  return <></>;
 };
 
 export default Scroll;
