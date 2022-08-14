852d686a6569a66e33967389979d6d668e8cb48d
Mesut <=>
2022-08-14 22:42:04 +0300

    contants files updated

--- src/Constants/contactConstants.js
+++ src/Constants/contactConstants.js
@@ -1,23 +1,22 @@
 export const contactConstants = [
     {
-        contactLink: "",
+        contactLink: "https://www.linkedin.com/in/mesut-cifci/",
         contactIcon: "fab fa-linkedin fa-2xl"
     },
     {
-        contactLink: "",
+        contactLink: "https://twitter.com/MesutCifci9",
         contactIcon: "fab fa-twitter-square fa-2xl"
     },
     {
-        contactLink: "",
+        contactLink: "https://github.com/mesutcifci",
         contactIcon: "fab fa-github fa-2xl"
     },
     {
-        contactLink: "",
+        contactLink: "https://stackoverflow.com/users/12382831/mesut-%c3%87ifci",
         contactIcon: "fab fa-stack-overflow fa-2xl"
     },
     {
-        contactLink: "",
+        contactLink: "https://mesutcifci.medium.com/",
         contactIcon: "fab fa-medium fa-2xl"
     },
 ]
-
