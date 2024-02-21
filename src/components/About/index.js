abd148354824a2ceb2c0414543cda4cb8c4a7ee3
Mesut Çiftçi <cfcmesut@gmail.com>
2024-02-21 18:38:23 +0300

    refactor(components): update about me text

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -11,39 +11,11 @@ const About = () => {
 
         <AboutMe>
           <p>
-            Passionate Frontend Developer with 2 years of professional
-            experience across three diverse industries, complemented by over
-            four years of dedicated self-learning and honing frontend
-            development skills. A self-taught enthusiast with a proven track
-            record of adapting swiftly to new code bases and contributing
-            effectively to various projects.
-          </p>
-          <p>
-            🚀Professional Expertise: Versatile experience in insurance,
-            education, and e-commerce sectors, demonstrating adaptability and
-            proficiency in diverse domains.
-          </p>
-          <p>
-            💻 Tech Stack: Proficient and familiar in a range of tools and
-            technologies including React, Vanilla JavaScript, CSS, Tailwind CSS,
-            SCSS, Material UI, Bootstrap, and more. Adept at leveraging the
-            right tools for optimal front-end development.
-          </p>
-          <p>
-            👩‍💻 Collaborative Contributor: Quick to acclimate to new environments
-            and contribute meaningfully to projects. Capable of diving into
-            complex code bases and making valuable contributions from the
-            get-go.
-          </p>
-          <p>
-            🔄 Feedback-Driven and Adaptive: Open to feedback and committed to
-            continuous improvement, always striving for excellence in coding
-            practices and project delivery.
-          </p>
-          <p>
-            🎸🎤 Beyond Coding: Beyond coding, I find joy in playing the guitar,
-            singing songs, and fostering a love for cats. A well-rounded
-            individual who brings creativity and passion to both code and life.
+            I am an experienced front-end developer building websites and web
+            applications mainly with JavaScript, React, and CSS. I also use
+            various tools and technologies such as SCSS, Tailwind CSS, Material
+            UI, Redux Toolkit, etc. Beyond code, I am a cat and anime lover and
+            I also play guitar and sing.
           </p>
         </AboutMe>
       </Wrapper>
